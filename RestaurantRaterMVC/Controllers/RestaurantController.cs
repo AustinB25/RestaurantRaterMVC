81450b044548bcd6e87c1b4f4feab63e89369047
Austin Bridgewater <abridgewater34@gmail.com>
2022-01-13 18:10:38 -0500

    initial commit

---
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -0,0 +1,109 @@
+﻿using RestaurantRaterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRaterMVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(_context.Restaurants.ToList());
+        }
+
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Post: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(restaurant);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        //Get: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+        //POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            //Find the selected restaurant
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            //Remove the restaurant from the database 
+            _context.Restaurants.Remove(restaurant);
+            //Save the changes to the database
+            _context.SaveChanges();
+            //Return to the index
+            return RedirectToAction("Index");
+        }
+        //GET: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id) 
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //Post: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+      public ActionResult Edit(Restaurant restaurant)
+        {
+            //Check to make sure restaurant is valid
+            if (ModelState.IsValid)
+            {
+                _context.Entry(restaurant).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        //GET: Restaurant/Details/{id}
+        public ActionResult etails(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _context.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+    }
+}
